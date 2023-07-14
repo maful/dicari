00cd0cb3965374191102408b8f1bb70a812d23c0
maful <me@maful.web.id>
2023-07-14 09:19:54 +0700

    chore(Job): add link to details page

--- app/app/jobs/page.tsx
+++ app/app/jobs/page.tsx
@@ -1,20 +1,15 @@
 "use client";
 
+import Link from "next/link";
 import type { Job } from "@prisma/client";
 import { useTable } from "@refinedev/core";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { CalendarDays, Users } from "lucide-react";
 
-import { Badge } from "@components/ui/badge";
+import { Badge, type BadgeProps } from "@components/ui/badge";
 import { Button } from "@components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@components/ui/card";
 import {
   PageHeaderDescription,
   PageHeaderHeading,
@@ -32,6 +27,49 @@ export default function Page() {
   // Checks if there is a previous page available
   const hasPrev = current > 1;
 
+  function renderJobBadge(job: Job): JSX.Element {
+    let variant: BadgeProps["variant"] = "default";
+    let title = "Active";
+
+    if (job.closed) {
+      variant = "destructive";
+      title = "Closed";
+    }
+
+    if (job.paused) {
+      variant = "outline";
+      title = "Paused";
+    }
+
+    return <Badge variant={variant}>{title}</Badge>;
+  }
+
+  function renderJobTime(job: Job): JSX.Element {
+    let time: JSX.Element = (
+      <>
+        {dayjs(job.createdAt).isSame(dayjs(job.updatedAt))
+          ? "created"
+          : "updated"}{" "}
+        {dayjs(job.updatedAt).fromNow()}
+      </>
+    );
+
+    if (job.paused) {
+      time = <>paused at {dayjs(job.pausedAt).format("MMMM D, YYYY h:mm A	")}</>;
+    }
+
+    if (job.closed) {
+      time = <>closed at {dayjs(job.closedAt).format("MMMM D, YYYY h:mm A	")}</>;
+    }
+
+    return (
+      <div className="flex items-center">
+        <CalendarDays className="mr-1 h-3 w-3" />
+        {time}
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="space-y-0.5">
@@ -39,34 +77,26 @@ export default function Page() {
         <PageHeaderDescription>View and manage all jobs</PageHeaderDescription>
       </div>
       <div className="pb-12 pt-8">
-        <div className="space-y-6">
+        <div className="flex flex-col gap-6">
           {jobs.map((job) => (
-            <Card key={job.id}>
-              <CardHeader className="flex flex-row items-start gap-4 space-y-0">
-                <div className="space-y-1 flex-1">
-                  <CardTitle>{job.title}</CardTitle>
-                </div>
-                <div>
-                  <Badge variant={job.paused ? "destructive" : "default"}>
-                    {job.paused ? "Paused" : "Active"}
-                  </Badge>
-                </div>
-              </CardHeader>
-              <CardContent>
-                <div className="flex space-x-4 text-sm text-muted-foreground">
-                  <div className="flex items-center">
-                    <Users className="mr-1 h-3 w-3" />0 candidates
+            <Link key={job.id} href={`/jobs/${job.publicId}`}>
+              <Card>
+                <CardHeader className="flex flex-row items-start gap-4 space-y-0">
+                  <div className="space-y-1 flex-1">
+                    <CardTitle>{job.title}</CardTitle>
                   </div>
-                  <div className="flex items-center">
-                    <CalendarDays className="mr-1 h-3 w-3" />
-                    {dayjs(job.createdAt).isSame(dayjs(job.updatedAt))
-                      ? "created "
-                      : "updated "}
-                    {dayjs(job.updatedAt).fromNow()}
+                  <div>{renderJobBadge(job)}</div>
+                </CardHeader>
+                <CardContent>
+                  <div className="flex space-x-4 text-sm text-muted-foreground">
+                    <div className="flex items-center">
+                      <Users className="mr-1 h-3 w-3" />0 candidates
+                    </div>
+                    {renderJobTime(job)}
                   </div>
-                </div>
-              </CardContent>
-            </Card>
+                </CardContent>
+              </Card>
+            </Link>
           ))}
         </div>
         <div className="flex items-center justify-end space-x-2 mt-6">
